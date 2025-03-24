f758e51b07fb788634f4184313ed36c21d8a8164
Ваше Ім'я <zhekakrochak79@gmail.com>
2025-03-24 19:26:59 +0200

    what

--- js/task-4.js
+++ js/task-4.js
@@ -1,18 +1,19 @@
-document.querySelector('.login-form').addEventListener('submit', function(event) {
+
+document.querySelector('.login-form').addEventListener('submit', function (event) {
     event.preventDefault();
-  
+
     const emailInput = this.elements['email'];
     const passwordInput = this.elements['password'];
-  
+
     if (emailInput.value.trim() === '' || passwordInput.value.trim() === '') {
-      alert('All form fields must be filled in');
+        alert('All form fields must be filled in');
     } else {
-      const formData = {
-        email: emailInput.value.trim(),
-        password: passwordInput.value.trim()
-      };
-  
-      console.log(formData);
-      this.reset();
+        const formData = {
+            email: emailInput.value.trim(),
+            password: passwordInput.value.trim()
+        };
+
+        console.log(formData);
+        this.reset();
     }
-  });
+});
--- js/task-5.js
+++ js/task-5.js
@@ -1,8 +1,14 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
 const button = document.querySelector('.change-color');
 const colorText = document.querySelector('span.color');
 
-button.addEventListener('click', function() {
-    const randomColor = getRandomHexColor();
-    document.body.style.backgroundColor = randomColor;
-    colorText.textContent = randomColor;
+button.addEventListener('click', function () {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorText.textContent = randomColor;
 });
